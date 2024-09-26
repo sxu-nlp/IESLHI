7c29f0b859028229f39925416fb41aee0a644bea
sxu-nlp <83936568+sxu-nlp@users.noreply.github.com>
2024-09-26 10:18:55 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# IESLHI
+
+### Environment
+
+```
+python (3.6.9)
+cuda (10.1)
+Ubuntu-18.0.4 or centos 7.0
+```
+
+### Dependencies
+
+```
+torch==1.6.0
+pytorch-pretrained-bert==0.4.0
+transformers==3.0.2
+numpy
+tensorboardX
+```
+
+### Train
+
+```
+$ cd scripts
+$ ./train_multi.sh
+```
+
+### Evaluation
+
+```
+$ cd scripts
+$ ./eval.sh
+```
+
+### Run
+```
+python run_dee_task.py
+```
---
+++ scripts/train_multi.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+NUM_GPUS=$1
+PRETRAINED_MODEL_DIR=$2
+shift
+
+python3  run_dee_task.py \
+--exp_dir ./Exps \
+--task_name RAAT \
+--gradient_accumulation_steps 8 \
+--train_batch_size 64 \
+--use_bert True \
+--bert_model 2 \
+--re_label_map_path ./Data/label_map.json \
+--logging_steps 100 \
+--eval_batch_size 32 \
+--learning_rate 5e-5 \
+--data_dir ./Data \
+--num_train_epochs 100 \
+--save_cpt_flag True \
+--raat True \
+--raat_path_mem True \
+--num_relation 18
+# -m torch.distributed.launch --nproc_per_node ${NUM_GPUS}
